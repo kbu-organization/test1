6e68fd45f580631d1619ade4021d9981ad59e6c9
2459005-dot <2459005@kbu.ac.kr>
2025-11-04 18:25:04 +0900

    myapp test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd test
+          Learn GithubAction cicd test change
         </a>
       </header>
     </div>
